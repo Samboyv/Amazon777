5eda6b2c63c451137e6dee32a69c155bab0965a1
sambo <sambo@DESKTOP-9MNODTU>
2021-05-22 13:38:28 -0400

    day3

---
+++ Areas/Usuarios/Paginas/Cuentas/Registrar.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amazon777.Areas.Usuarios.Paginas.Cuentas
+{
+    public class RegistrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
